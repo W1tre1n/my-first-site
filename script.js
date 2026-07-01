ebb80eb3f4955d705a9943cffe90bb45f3ac3630
W1tre1n <alelisseew26@gmail.com>
2026-07-01 22:04:04 +0400

    добавил страницу профиля

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('myButton').addEventListener('click', () => {
     alert('Сайт работает!');
 });
+document.getElementById('profileButton').addEventListener('click', () => {
+    window.location.href = 'profile.html'; // Это команда переадресации
+});
